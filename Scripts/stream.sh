c84c246f11281b4498be8892393cf4c273f94318
Éderson Cássio <3098527+EdyKnopfler@users.noreply.github.com>
2023-01-07 15:27:10 -0300

    Usar VLC dá melhor desempenho!
    O vídeo abre mais rápido e não perdemos a interação com mouse :)

--- Scripts/stream.sh
+++ Scripts/stream.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 URL=$(xclip -selection c -o)
-mplayer -fs -cookies -cookies-file /tmp/cookie.txt \
-    $(./yt-dlp -gf 18 --cookies /tmp/cookie.txt  "$URL")
+vlc $(./yt-dlp -gf 18 --cookies /tmp/cookie.txt  "$URL")
